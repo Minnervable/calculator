dc80dd1bcab2ea1fc226c873273e09943d0a626f
Minnerva Garrett <267438469+Minnervable@users.noreply.github.com>
2026-05-25 23:26:20 -0400

    Cap the screen, add comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// these four functions are the basis of the computer
+
 function add(firstNumber, secondNumber) {
 	return firstNumber + secondNumber;
 }
@@ -13,7 +15,7 @@ function multiply(firstNumber, secondNumber) {
 function divide(firstNumber, secondNumber) {
 	return firstNumber / secondNumber;
 }
-
+// global variables
 const infinity = [
 	"And then chaos ensued",
 	"A black hole",
@@ -26,15 +28,15 @@ let operator;
 let finalNumber;
 let phaseTracker = "idle";
 let newNumber = true;
-
+// this function uses the other four functions inside it to make the computer do math
 function operate(op, firstNumber, secondNumber) {
 	switch (op) {
 		case "+":
-			return +add(firstNumber, secondNumber).toFixed(8);
+			return +add(firstNumber, secondNumber).toFixed(15);
 		case "-":
-			return +subtract(firstNumber, secondNumber).toFixed(8);
+			return +subtract(firstNumber, secondNumber).toFixed(15);
 		case "*":
-			return +multiply(firstNumber, secondNumber).toFixed(8);
+			return +multiply(firstNumber, secondNumber).toFixed(15);
 		case "/":
 			if (secondNumber === 0) {
 				let currentJoke = infinity[infinityCounter];
@@ -44,13 +46,17 @@ function operate(op, firstNumber, secondNumber) {
 				}
 				return currentJoke;
 			} else {
-				return +divide(firstNumber, secondNumber).toFixed(8);
+				return +divide(firstNumber, secondNumber).toFixed(15);
 			}
 	}
 }
+// this line create button array
 const buttons = document.querySelectorAll(".button");
+// this line creates the function to loop through the buttons
 buttons.forEach(function (button) {
+	// this block covers possible conditions the computer could encounter
 	button.addEventListener("click", function () {
+		// the blink function makes the calculator blink every time a button pressed
 		function blink(screen = "0") {
 			display.textContent = "";
 			setTimeout(function () {
@@ -58,6 +64,7 @@ buttons.forEach(function (button) {
 			}, 32);
 			return;
 		}
+		// this block controls most of the decimal functionality
 		if (button.id === "decimal") {
 			button.classList.add("disabled");
 			if (newNumber === true) {
@@ -69,17 +76,23 @@ buttons.forEach(function (button) {
 				return;
 			}
 		}
+		// this block controls the number buttons and the decimal sometimes
 		if (button.classList.contains("number") || button.id === "decimal") {
 			if (newNumber === true) {
 				display.textContent = "";
 				newNumber = false;
 			}
-			display.textContent = display.textContent + button.textContent;
+			// this snippet caps the screen so it doesnt overflow
+			if (display.textContent.length < 16) {
+				display.textContent = display.textContent + button.textContent;
+			}
 		}
+		// this snippet refuses to let the decimal button be pressed at all if its disabled
 		if (button.classList.contains("disabled")) {
 			return;
 		}
 
+		// this block mainly controls the basic new number flag variable. it only triggers when a number is pressed
 		if (
 			newNumber === true &&
 			!button.classList.contains("operator") &&
@@ -89,6 +102,7 @@ buttons.forEach(function (button) {
 			display.textContent = "";
 			newNumber = false;
 		}
+		// this block controls the all clear button
 		if (button.id === "all-clear") {
 			if (display.textContent !== "") {
 				display.textContent = "0";
@@ -100,6 +114,7 @@ buttons.forEach(function (button) {
 				document.querySelector("#decimal").classList.remove("disabled");
 			}
 		}
+		// this block controls the backspace button
 		if (button.id === "backspace") {
 			if (display.textContent !== "") {
 				display.textContent = display.textContent.slice(0, -1);
@@ -114,6 +129,7 @@ buttons.forEach(function (button) {
 				finalNumber = display.textContent;
 			}
 		}
+		// this block controls the change sign button
 		if (button.id === "change-sign") {
 			display.textContent = display.textContent * -1;
 			if (phaseTracker === "firstNumber") {
@@ -125,19 +141,24 @@ buttons.forEach(function (button) {
 				finalNumber = display.textContent;
 			}
 		}
+		// this block controls what state the calculator is in
 		switch (phaseTracker) {
+			// if the state is idle the screen is on 0 and no variables have been assigned
 			case "idle":
 				if (button.classList.contains("number")) {
 					display.textContent = button.textContent;
 					phaseTracker = "firstNumber";
 				}
+				// if the first button pressed is the decimal there will be a 0 placed in from of the decimal
 				if (button.id === "decimal") {
 					phaseTracker = "firstNumber";
 					display.textContent = "0" + button.textContent;
 				}
 				break;
 
+			// if the state is first number the initial number will be saved
 			case "firstNumber":
+				// if the operator is not undefined then the math will run on the screen
 				if (button.classList.contains("operator")) {
 					if (operator !== undefined) {
 						display.textContent = operate(
@@ -146,6 +167,7 @@ buttons.forEach(function (button) {
 							+display.textContent,
 						);
 					}
+					// these next lines happen any time an operator is hit
 					operator = button.textContent;
 					phaseTracker = "secondNumber";
 					initialNumber = display.textContent;
@@ -157,7 +179,9 @@ buttons.forEach(function (button) {
 
 				break;
 
+			// this block controls the second number
 			case "secondNumber":
+				// if the first number pressed is equals
 				if (button.id === "equals") {
 					finalNumber = display.textContent;
 					display.textContent = operate(
@@ -173,6 +197,7 @@ buttons.forEach(function (button) {
 					document
 						.querySelector("#decimal")
 						.classList.remove("disabled");
+					// if the fist button pressed is an operator
 				} else if (button.classList.contains("operator")) {
 					if (newNumber === true) {
 						operator = button.textContent;
@@ -194,10 +219,11 @@ buttons.forEach(function (button) {
 				}
 				break;
 		}
+		// calling blink at the end of all the other code for this listener ensures that it runs for every button press
 		blink(display.textContent);
 	});
 });
-
+// this block is for the keyboard support. one of the cases triggers every time one of the assigned buttons is pressed
 window.addEventListener("keydown", function (event) {
 	let key = event.key;
 	let buttonId;
@@ -260,6 +286,7 @@ window.addEventListener("keydown", function (event) {
 			buttonId = "#change-sign";
 			break;
 	}
+	// this controls the flashing of the buttons when the keyboard is being used
 	if (buttonId !== undefined) {
 		let buttonToClick = this.document.querySelector(buttonId);
 		buttonToClick.classList.add("pressed");
